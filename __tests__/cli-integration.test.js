e9c0eff0352c10630fc826311bb320f2c4361f1c
Tom Davies <tom@thenuel.com>
2020-09-30 23:04:51 +0100

    Adds basic test for verifying "stitch" behaviour

--- __tests__/cli-integration.test.js
+++ __tests__/cli-integration.test.js
@@ -2,13 +2,13 @@ const { system, filesystem } = require('gluegun');
 
 const cwd = filesystem.path(__dirname, '..');
 
-const cli = async cmd =>
+const cli = app => async cmd =>
   system.run('node ' + filesystem.path(cwd, 'bin', 'next-stitch') + ` ${cmd}`, {
-    cwd: filesystem.path(cwd, 'example')
+    cwd: filesystem.path(cwd, app)
   });
 
 test('prepare pages', async () => {
-  const output = await cli('prepare pages');
+  const output = await cli('example')('prepare pages');
   expect(output).toContain('pages bundle created');
 
   expect(
@@ -16,4 +16,20 @@ test('prepare pages', async () => {
   ).toMatchSnapshot();
 
   filesystem.remove('./example/dist');
+});
+
+test('stitch', async () => {
+  const output = await cli('example-shell')('stitch');
+  expect(output).toContain('stitched "@next-stitch/next-stitch-test-package"');
+
+  expect(
+    filesystem.find('./example-shell/src/pages', { matching: '**/*' })
+  ).toMatchSnapshot();
+
+  expect(
+    filesystem.read('./example-shell/src/pages/about.ts')
+  ).toMatchSnapshot();
+
+  filesystem.remove('./example-shell/pages/nested');
+  filesystem.remove('./example-shell/pages/about.ts');
 });
