afe405f347b7cb86c5e6d938e7ec048873d2e283
Tom Davies <tom@thenuel.com>
2020-09-30 22:55:09 +0100

    Adds ISR demo to example package

--- example/src/pages/about.tsx
+++ example/src/pages/about.tsx
@@ -1,9 +1,20 @@
 import StyledButton from '../components/StyledButton';
 
-const AboutPage = () => (
+const AboutPage = ({ generatedAt }) => (
   <div>
     <h1>About</h1>
     <StyledButton />
+    <p>Generated at: {generatedAt}</p>
   </div>
 );
+
+export async function getStaticProps() {
+  return {
+    props: {
+      generatedAt: new Date(Date.now()).toISOString()
+    },
+    revalidate: 5
+  };
+}
+
 export default AboutPage;
